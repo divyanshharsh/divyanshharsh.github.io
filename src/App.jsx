e3233707a412cd189f0cda4d0d6b4cf3526ab13d
divyanshharsh <divyansh.feb2@gmail.com>
2025-03-30 19:28:23 +0530

    Fixed routing issue with HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Import React Router components
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import ContentSection from './components/ContentSection'; // Main page component
+import ContentSection from './components/ContentSection';
 import Sidebar from './components/Sidebar';
 import Footer from './components/Footer';
 
-// Import placeholder components for other pages
 import BookPage from './components/BookPage';
 import BlogPostPage from './components/BlogPostPage';
 import TalksPage from './components/TalksPage';
@@ -16,14 +15,14 @@ import AIPlaybookPage from './components/AIPlaybookPage';
 
 function App() {
   return (
-    <Router>
+    <Router>  {/* Changed to HashRouter */}
       <div className="min-h-screen flex flex-col bg-gray-50">
         <Navbar />
         <main className="flex-grow container mx-auto px-4 pt-24 pb-8">
           <div className="flex flex-col md:flex-row gap-8">
             <Sidebar />
             <div className="flex-grow">
-              <Routes> 
+              <Routes>
                 <Route path="/" element={<ContentSection />} />
                 <Route path="/pages/book" element={<BookPage />} />
                 <Route path="/pages/blog-post" element={<BlogPostPage />} />
