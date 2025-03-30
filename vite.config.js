9548dcd295a78549a35aae1d1e9ca87e95577f52
Divyansh Harsh <90635122+divyanshharsh@users.noreply.github.com>
2025-03-30 19:57:15 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/gasir/', // Must match GitHub repo name exactly
+  base: '/', // Must match GitHub repo name exactly
 });
